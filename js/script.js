5755c1ba17f443c78d101c91f72cd63d8f379050
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-10-27 10:29:40 +0200

    [add] function stamp html

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-/*
-Riprendiamo l’esercizio carosello e rifacciamolo, questa volta usando un array di oggetti.
-Ogni elemento deve avere un titolo, una descrizione e il riferimento ad una immagine.
-Le immagini devono essere 5 e nella grafica devono essere presenti:
-- immagine in evidenza
-- thumbnail di tutte le immagine con in evidenza l’immagine attiva
-- bottone avanti e indietro
-**Bonus 1:**
-Sperimentiamo attraverso l’uso delle timing functions anche una funzionalità di scorrimento al nostro carosello:al click di un bottone o già dall’inizio possiamo far partire, ad intervalli di tempo a piacere, lo scorrimento delle immagini disponibili nel carosello stesso.
-****
-**Bonus 2:**
-E se volessi un bottone per invertire la “direzione” del carosello?
-****
-*/
-
 const immagini = [
   {
     nome: "Argentina",
@@ -49,24 +34,7 @@ const play = document.getElementById('btn-start');
 const invert = document.getElementById('btn-invert');
 
 immagini.forEach((immagine) =>{
-
-  let imgSlider = `
-    <div class="text">
-      <h4>${immagine.nome}</h4>
-      <p>${immagine.descrizione}</p>
-    </div>
-
-    <img class="items" src="img/${immagine.pic}"  alt="${immagine.nome}">
-  `;
-
-  let thumbSlider = `
-    <div class="thumbnail-img">
-      <img src="img/${immagine.pic}" alt="${immagine.nome}">
-    </div>
-  `;
-
-  imgWrapper.innerHTML += imgSlider;
-  thumbWrapper.innerHTML += thumbSlider;
+  getTemplate(immagine)
 })
 
 let counter = 0;
@@ -97,6 +65,29 @@ nextBtn.addEventListener('click', function(){
 prevBtn.addEventListener('click', function(){
   next(isNext);
 })
+
+
+function getTemplate(immagine) {
+  const {nome, descrizione, pic} = immagine;
+
+  let imgSlider = `
+    <div class="text">
+      <h4>${nome}</h4>
+      <p>${descrizione}</p>
+    </div>
+
+    <img class="items" src="img/${pic}"  alt="${nome}">
+  `;
+
+  let thumbSlider = `
+    <div class="thumbnail-img">
+      <img src="img/${pic}" alt="${nome}">
+    </div>
+  `;
+
+  imgWrapper.innerHTML += imgSlider;
+  thumbWrapper.innerHTML += thumbSlider;
+}
 
 function next(isNext){
   element[counter].classList.remove('active');
